ff654020ec8141f424570d9d1e3077926febbc26
Andrevfcb <amajewski1994@gmail.com>
2025-01-23 15:41:22 +0100

    login & register form

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,33 @@
 import Home from "./Components/Home"
 import Layout from "./Components/Layout"
 import Navbar from "./Components/Navbar"
+import Modal from "./Components/Modal"
+import { useState } from "react"
 
 function App() {
+
+  const [modalIn, setModalIn] = useState(false)
+  const [modalForm, setModalForm] = useState(false)
+
+  const openModal = () => {
+    setModalIn(true)
+  }
+
+  const closeModal = () => {
+    setModalIn(false)
+  }
+
+  const switchModal = () => {
+    setModalForm(prevModalForm => !prevModalForm)
+  }
+
   return (
     <>
       <Layout />
       <div className=" absolute top-0 left-0 box-border h-dvh w-dvw">
-        <Navbar />
+        <Navbar openModal={openModal} />
         <Home />
+        <Modal isRegisterForm={modalForm} modalIn={modalIn} closeModal={closeModal} switchModal={switchModal} />
       </div>
     </>
   )
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,4 +1,4 @@
-const Navbar = () => {
+const Navbar = ({ openModal }) => {
     return (
         <div className="w-full min-h-20 bg-gradient-to-r from-cyan-500 to-blue-500 flex justify-center items-center shadow-sm shadow-black">
             <div className="text-2xl font-bold flex-1 text-center text-amber-100">
@@ -7,8 +7,11 @@ const Navbar = () => {
                 </h2>
             </div>
             <div className="mx-10 text-xl text-white">
-                <div className="cursor-pointer border-b-2 border-b-transparent rounded-sm transition-colors duration-500 [text-shadow:_1px_1px_0_rgb(0_0_0_/_40%)] hover:border-b-white">
-                    Register
+                <div
+                    className="cursor-pointer border-b-2 border-b-transparent rounded-sm transition-colors duration-500 [text-shadow:_1px_1px_0_rgb(0_0_0_/_40%)] hover:border-b-white"
+                    onClick={openModal}
+                >
+                    Login
                 </div>
             </div>
         </div>
