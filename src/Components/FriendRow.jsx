36ced634fa9b4084adf68c8cc1252df781bc67ae
Andrevfcb <amajewski1994@gmail.com>
2025-02-19 15:39:15 +0100

    friends image fix

--- src/Components/FriendRow.jsx
+++ src/Components/FriendRow.jsx
@@ -8,7 +8,7 @@ const FriendRow = ({ _id, image, firstName, lastName, online, chengeActiveFriend
     return (
         <li className="flex text-start items-center cursor-pointer transition-all rounded my-2 hover:bg-slate-200/25" onClick={clickHandler}>
             <div className="p-2">
-                <img src={'../../public/bg-image.jpg'} alt='avatar' className="w-10 h-10 rounded-full" />
+                <img src={`https://chat-app-andrev.s3.eu-central-1.amazonaws.com/${image}`} alt='avatar' className="w-10 h-10 rounded-full" />
             </div>
             <div className="p-2">
                 <div className="text-white [text-shadow:_1px_1px_0_rgb(0_0_0_/_40%)]">{firstName} {lastName}</div>
